fbddd1ff986cc185bd4c6a2ecc201371908e6005
Junior Paixao <juniortidesenvolvimento1@gmail.com>
2023-01-20 15:35:00 -0300

    UP
    up

--- index.js
+++ index.js
@@ -2,11 +2,45 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const produtos = require("./src/produtos/produtos.json")
+const alunos = require("./src/alunos/alunos.json")
 
-app.get("/produtos", (req, res) => {
-    return res.json(produtos)
+// retornar todos os alunos
+app.get("/alunos", (req, res) => {
+    return res.json(alunos)
 })
+
+// retornar um aluno
+app.get("/alunos/:index", (req, res) => {
+    const { index } = req.params;
+    return res.json(alunos[index]);
+})
+
+// cadastrando um aluno
+app.post('/alunos', (req, res) => {
+    const { nome } = req.body;
+    alunos.push({ nome })
+
+    return res.json(alunos)
+})
+
+// atualizar um aluno
+app.put('/alunos/:index', (req, res) => {
+    const { index } = req.params;
+    const { nome, email } = req.body;
+
+    alunos[index] = (nome);
+
+    return res.json(alunos);
+})
+
+// deletar um aluno
+app.delete('/alunos/:index', (req, res) => {
+    const { index } = req.params;
+
+    alunos.splice(index, 1);
+    return res.json({ message: "O alunos foi deletado com sucesso!" })
+})
+
 
 app.listen(port, () => {
     console.log('Servidor está rodando...');
