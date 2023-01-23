236938f726b10f254632c6eba08cbe86dec90937
Junior Paixao <juniortidesenvolvimento1@gmail.com>
2023-01-23 11:12:23 -0300

    api funcional
    finalizado

--- index.js
+++ index.js
@@ -1,47 +1,67 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+var bodyParser = require('body-parser')
+
+//
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//
+
 
 const alunos = require("./src/alunos/alunos.json")
 
 // retornar todos os alunos
 app.get("/alunos", (req, res) => {
-    return res.json(alunos)
+    res.json(alunos)
 })
 
-// retornar um aluno
+// retornar um aluno por id
 app.get("/alunos/:index", (req, res) => {
-    const { index } = req.params;
-    return res.json(alunos[index]);
+    let aluno = alunos.find(a => a.id === req.params.index);
+    if (!aluno) {
+        res.status(404).json({ message: "Aluno não encontrado" });
+    } else {
+        res.json(aluno);
+    }
 })
 
 // cadastrando um aluno
 app.post('/alunos', (req, res) => {
-    const { nome } = req.body;
-    alunos.push({ nome })
+    let newAlunos = {
+        id: alunos.length + 1,
+        nome: req.body.nome,
+        email: req.body.email
+    }
 
+    alunos.push(newAlunos);
     return res.json(alunos)
 })
 
 // atualizar um aluno
 app.put('/alunos/:index', (req, res) => {
-    const { index } = req.params;
-    const { nome, email } = req.body;
-
-    alunos[index] = (nome);
-
-    return res.json(alunos);
+    let aluno = alunos.find(a => a.id === req.params.index);
+    if (!aluno) {
+        res.status(404).json({ message: "Aluno não encontrado" });
+    } else {
+        aluno.nome = req.body.nome;
+        aluno.email = req.body.email;
+        res.json(aluno);
+    }
 })
 
 // deletar um aluno
 app.delete('/alunos/:index', (req, res) => {
-    const { index } = req.params;
-
-    alunos.splice(index, 1);
-    return res.json({ message: "O alunos foi deletado com sucesso!" })
+    let alunoIndex = alunos.findIndex(a => a.id === req.params.index);
+    if (alunoIndex == -1) {
+        res.status(404).json({ message: "Aluno não encontrado" });
+    } else {
+        let deletarAluno = alunos.splice(alunoIndex, 1);
+        res.json(deletarAluno);
+    }
 })
 
-
+// conexão com o servidor
 app.listen(port, () => {
     console.log('Servidor está rodando...');
 })
